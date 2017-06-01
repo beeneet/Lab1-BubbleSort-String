b8f5e653fd5619e35b4e491fdf50e53c3a0891e2
Binit Koirala <bintskoirala@gmail.com>
2017-06-01 11:36:06 -0700

    Added codes for taking input from the user.

--- lab1.cc
+++ lab1.cc
@@ -1,4 +1,4 @@
-// Simple Hello World
+// Simple Hello World and bubble sort
  
 #include <iostream>
 #include <string>
@@ -7,18 +7,28 @@ using namespace std;
 void bubbleSort(string[] , int ); 
 int main()
 {
-
+  string usrInput = "";
+  string name;
+  int count = 0;
   cout << "Hello World!" << endl;
   cout << "New line I added." << endl;
-  string array[5] = {"Music", "Maps", "Store", "Chrome", "Games"};
-  bubbleSort(array, sizeof(array)/sizeof(array[0]));
+  cout << "This program sorts the string" << endl;
+  cout << "Please enter number of entries: ";
+  cin >> count;
+  string array[count];
+  cout << "Please enter the names: " << endl;
+  int i = 0;
+  while (i<count){
+      cin >> array[i];
+      i++;
+  }
+  bubbleSort(array, count);
   return 0;
 }
 
 void bubbleSort(string array[], int len) {
-  
   for (int i = 0; i < len; i++){
-      for (int j = 0; j < len-1; j++){
+      for (int j = 0; j < len-i-1; j++){
           if (array[j] > array[j+1]) {
               string temp = array[j];
               array[j] = array[j+1];
@@ -26,9 +36,9 @@ void bubbleSort(string array[], int len) {
           } 
       }
   }
-  
+  cout << "The sorted names are: ";
   for (int k = 0; k < len; k++){
       cout << array[k] << " ";
   }
-  
+  cout << endl;
 } 
